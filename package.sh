26a85f8f35ce77f40441c213a0b7f99a1764dfe5
Paolo Zaccagnini <paolo@pzac.net>
2026-02-28 08:12:10 +0100

    Add package.sh script

---
+++ package.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <tag>"
+  exit 1
+fi
+
+TAG="$1"
+ARCHIVE="${TAG}.tar.gz"
+
+tar czf "$ARCHIVE" \
+  --exclude="./.git" \
+  --exclude="./.jj" \
+  --exclude="./target" \
+  --exclude="./.claude" \
+  --exclude="./*.tar.gz" \
+  .
+
+shasum -a 256 "$ARCHIVE"
